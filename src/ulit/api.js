e5d52f3cf654204da98a0227044d138488378177
qu tang <takeern@gmail.com>
2019-04-26 11:20:18 +0800

    [update]

--- src/ulit/api.js
+++ src/ulit/api.js
@@ -1,5 +1,5 @@
 const develement = 'uat';
-const host = develement === 'dev' ? '//localhost:4000/' : 'http://10.23.30.131:4000/';
+const host = develement === 'dev' ? '//localhost:4000/' : 'http://47.103.12.134:4000/';
 export default {
     getBookInitData: host + 'getBookInitData?bookNumber=',
     getBookData: host + 'getBookData',
