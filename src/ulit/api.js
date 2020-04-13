55c5f59b7405eb6b6ffe494d53cd3399fe13840b
takeern <takeern@takeerndeMac-mini.local>
2020-04-13 10:57:44 +0800

    [update] api

--- src/ulit/api.js
+++ src/ulit/api.js
@@ -1,8 +1,8 @@
-const develement = 'dev';
-const host = develement === 'dev' ? 'http://47.103.12.134:8083/' : 'http://127.0.0.1:8083/';
+const develement = 'prod';
+const host = develement === 'dev' ? 'http://112.74.110.72:8083/' : 'http://127.0.0.1:8083/api/';
 export default {
     getBookInitData: host + 'getBookInitData?bookNumber=',
     getBookData: host + 'getBookData',
     searchBook: host + 'getBookNumber?bookName=',
-    downloadBook: '47.103.12.134:4536/',
+    downloadBook: '112.74.110.72:4536/',
 };
